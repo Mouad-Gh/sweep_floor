864b35da6c858e29c5e4ca74b92e96b078f2dc2d
Mouad-Gh <Mouad.ghouichat@gmail.com>
2023-03-18 13:15:57 +0100

    adding the logic for the gift & the confetti animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Navbar from './components/Navbar'
 import FloorCard from './components/FloorCard'
 import useFetch from './hooks/useFetch'
@@ -17,9 +17,19 @@ function App() {
   }
 
   
+  //
+  // function handleWindowSize(){
+  //   setWindowSize({
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   });
+  // }
+  //stop the confetti animation
+  
   return (
     <div className="pb-8">
       <Navbar walletAddress={walletAddress} setWalletAddress={setWalletAddress} />
+      
       <section className="container p-8 mx-auto xl:px-0 relative">
         <div className="w-full max-h-[70vh] py-0 md:py-[6%] flex flex-col md:flex-row md:justify-evenly gap-2 md:gap-0 ">
           <div className="text-left md:py-[5%] selection:bg-black selection:text-white">
@@ -32,7 +42,8 @@ function App() {
           </div>
         </div>
       </section>
-      <section className="w-full min-h-screen flex flex-col items-center justify-center" ref={myDiv}>
+      <section className="w-full relative min-h-screen flex flex-col items-center justify-center" ref={myDiv}>
+        
         <h2 className="mt-20 lg:mt-10 text-center text-3xl">Buy the floor</h2>
         <p className="py-4 text-base mt-2 max-w-[460px] text-center text-[#707070]">Take a look at the charming scene of apes relaxing on the ground in stargaze.zone - sweep to see magic.
 </p>
--- src/components/FloorCard.jsx
+++ src/components/FloorCard.jsx
@@ -1,9 +1,50 @@
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 
 const FloorCard = ({ask, walletAddress}) => {
+    const [isLoading, setLoading] = useState(false);
+    const [txHash, setTxHash] = useState(null);
     //the sweep button
-    const handleSweep = async() => {
+    // console.log(isLoading);
+    const [showConfetti, setShowConfetti ] = useState(false);
+    const [buttonPosition, setButtonPosition ] = useState({
+        x: 0,
+        y: 0
+    });
+    const [dimensions, setDimensions] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    useEffect(() => {
+        function handleResize() {
+          setDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+      useEffect(()=>{
+        // window.onresize = () => handleWindowSize();
+        showConfetti && setTimeout(() => {
+          setShowConfetti(false)
+        }, 8000);
+      }, [showConfetti]);
+
+    // console.log(showConfetti, buttonPosition);
+
+    const handleSweep = async(event) => {
+        try {
+            
+        
+        setLoading(true);
         let mnemonic = import.meta.env.VITE_MNEMONIK;
         let nftContractAddress = import.meta.env.VITE_NFT_CONTRACT_ADDRESS;
 
@@ -62,7 +103,49 @@ const FloorCard = ({ask, walletAddress}) => {
             fee
         );
         
-        console.log(tx);
+        if(tx){
+            console.log("*******************************************************",tx);
+            //send
+            const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'stars' });
+            const [firstAccount] = await wallet.getAccounts();
+            const rpcEndpoint = 'https://rpc.stargaze-apis.com';
+            const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet); 
+            // console.log("address", firstAccount.address,walletAddress, client  )
+            
+
+            const tx2 = await client.signAndBroadcast(
+                firstAccount.address,
+                [
+                    {
+                        typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+                        value: {
+                            fromAddress: firstAccount.address,
+                            toAddress: walletAddress,
+                            amount: [
+                                {
+                                    denom: 'ustars',
+                                    amount: parseInt(ask.price * 0.05 ).toString(),
+                                }
+                            ],
+                        }
+                    }
+                ], 
+                fee
+            );
+            console.log("tx22222222222222222 ", tx2);
+            setTxHash(tx2.transactionHash);
+            setLoading(false);
+            //animation
+            const buttonRect = event.target.getBoundingClientRect();
+            setButtonPosition({ x: buttonRect.left + buttonRect.width / 2, y: buttonRect.top + buttonRect.height / 2 });
+            setShowConfetti(true);
+        }
+        
+    } catch (error) {
+        setLoading(false);
+        console.log(error);   
+    }
+
         // console.log(import.meta.env.VITE_NFT_CONTRACT_ADDRESS)
     }
     //generate the image url using the object token_id var
@@ -86,15 +169,41 @@ const FloorCard = ({ask, walletAddress}) => {
                                 </div>
                             </div>
                         </div>
-                        <div className="relative -translate-y-1 cursor-pointer transition ease-in-out hover:-translate-y-2 hover:scale-105 duration-300">
+                        <div className="relative select-none -translate-y-1 cursor-pointer transition ease-in-out hover:-translate-y-2 hover:scale-105 duration-300">
                             <div className="rounded-md w-full bg-gradient-to-r p-[2px] from-[#7928ca] to-[#ff0080]">
+                                    {txHash ?
+                                        ( <div className="flex items-center justify-center gap-x-1 sm:gap-2 h-full py-1 bg-black text-white rounded-md">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-green-500">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
+                                            <p className="text-[#e5e5e5] text-[10px] sm:text-xs">View transaction in explorer <a href={"https://www.mintscan.io/stargaze/txs/"+txHash}  target="_blank" rel="noopener noreferrer" className="w-4 h-4"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 inline-flex"><path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg></a>.</p>
+
+                                        </div> )
+                                    :
                                 <div data-tip="Connect your wallet first." className={`relative flex flex-col justify-between items-center h-full bg-black text-white rounded-md ${(!walletAddress) &&  'hover:disableSpan'}`}>
-                                    <button  disabled={(!walletAddress)} onClick={handleSweep} className="text-xl wallet-adapter-button !bg-black !text-center flex justify-center items-center w-full !px-2 !h-8 md:!h-10 disabled:cursor-not-allowed">Sweep 🧹</button>
-                                </div>
+                                        <button  disabled={(!walletAddress)} onClick={handleSweep} className="text-[10px] sm:text-xs wallet-adapter-button !bg-black !text-center flex justify-center items-center w-full !px-2 min-h-[1.5rem] disabled:cursor-not-allowed">
+                                        {isLoading ? <>sweeping... <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="animate-spin ml-1 h-5 w-5 text-white" viewBox="0 0 24 24" >
+                                            <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" className="opacity-25" ></circle>
+                                            <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" className="opacity-75" ></path>
+                                        </svg></> : <>Sweep 🧹</>}
+                                        </button>
+                                    
+                                    
+                                </div>}
                             </div>
                         </div>
                     </div>
                 </div>
+                {showConfetti && (<div className="fixed inset-0">
+                    <Confetti 
+                        width={dimensions.width}
+                        height={dimensions.height}
+                        numberOfPieces={100}
+                        recycle={false}
+                        confettiSource={{ x: buttonPosition.x, y: buttonPosition.y, w: 0, h: 0 }}
+                    />
+                    </div>)
+                }
                 <div className="pb-3 pr-1 text-[12px] w-full ">
                     <span className="block w-fit ml-auto text-[#70707080]">powred by <a href="https://apello.xyz" target="_blank" rel="noopener noreferrer" className="text-violet">Apello</a></span>
                 </div>
